4d119734b82b5d940ed9e6632f04a69c7ee975d9
Goodween <gundalini335@gmail.com>
2025-12-25 14:52:04 +0300

    fixed cmake in the root, and in two main directories. fixed the visibility of the Game and Engine at the CMake level. updated .gitignore

--- src/Game/Main.cpp
+++ src/Game/Main.cpp
@@ -3,21 +3,10 @@
 
 int main()
 {
-	try
-	{
-		Tassathras::Application app;
-		app.run();
-	}
-	catch (const std::exception& e)
-	{
-		std::cerr << "unhandled exception: " << e.what() << std::endl;
-		return -1;
-	}
-	catch(...)
-	{
-		std::cerr << "unkonwn exception.\n";
-		return -1;
-	}
+
+	Tassathras::Application app;
+	app.run();
+
 
 	return 0;
 }
